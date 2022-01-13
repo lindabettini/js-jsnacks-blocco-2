4ac658181d870df5a116b8a631e6a69920d08c18
lindabettini <linda.bettini@hotmail.it>
2022-01-13 15:50:37 +0100

    Primo svolgimento

--- script2.js
+++ script2.js
@@ -2,3 +2,11 @@
 todo - Inserisci un numero, se è pari stampa il numero,
 todo - se è dispari stampa il numero successivo.
 */
+
+const askNumb = parseInt(prompt('Inserisci un numero'));
+
+if (askNumb % 2){
+    console.log(askNumb+1);
+} else {
+    console.log(askNumb);
+}

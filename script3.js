65ae3c2f91d0c135ab9b04204f6845eb668815db
lindabettini <linda.bettini@hotmail.it>
2022-01-13 17:16:37 +0100

    Add versione FOR

--- script3.js
+++ script3.js
@@ -3,6 +3,31 @@ todo - Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e u
 todo - lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 */
 
+// // VERSIONE FOR
+const fakeInvites = [];
+const firstNames = ['Linda', 'Laura', 'Adriana', 'Francesca', 'Sara', 'Luisa','Giuditta', 'Federica', 'Cristina'];
+const lastNames = ['Bettini', 'Bigoni', 'Rossi', 'Verdi', 'Bonini', 'Giuliani', 'Nannini'];
+
+for (let i = 0; fakeInvites.length < 3; i++) {
+    const randNum1 = Math.floor(Math.random() * firstNames.length);
+    const randNum2 = Math.floor(Math.random() * lastNames.length);
+
+    const name = firstNames[randNum1];
+    const lastname = lastNames[randNum2];
+
+    const fakePerson = `${name} ${lastname}`;
+
+    console.log('fake person:', fakePerson);
+
+    if (!fakeInvites.includes(fakePerson)) {
+        fakeInvites.push(fakePerson);
+    }
+} 
+
+console.table(fakeInvites);
+
+/*
+// // VERSIONE WHILE
 const fakeInvites = [];
 const firstNames = ['Linda', 'Laura', 'Adriana', 'Francesca', 'Sara', 'Luisa','Giuditta', 'Federica', 'Cristina'];
 const lastNames = ['Bettini', 'Bigoni', 'Rossi', 'Verdi', 'Bonini', 'Giuliani', 'Nannini'];
@@ -24,3 +49,4 @@ while (fakeInvites.length < 3) {
 } 
 
 console.table(fakeInvites);
+*/

601018cd00f7a8baeee286ebebfcddce055ee04c
lindabettini <linda.bettini@hotmail.it>
2022-01-14 09:49:03 +0100

    versione do while

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ todo - Inserisci un numero, se è pari stampa il numero,
 todo - se è dispari stampa il numero successivo.
 */
 
-
+/*
 let askNumb = parseInt(prompt('Inserisci un numero'));
 
 if (askNumb % 2){    
@@ -11,7 +11,7 @@ if (askNumb % 2){
 } else {
     console.log(askNumb);
 }
-
+*/
 
 /*
 // ° VERSIONE WHILE
@@ -24,3 +24,18 @@ while (askNumb % 2) {
 
 console.log(askNumb)
 */
+
+// // VERSIONE DO WHILE
+
+let askNumb; 
+
+do {
+    askNumb = parseInt(prompt('Inserisci un numero'));
+} while (isNaN(askNumb) || askNumb < 0);
+
+
+if (askNumb % 2 === 0) {
+    console.log(askNumb);
+} else {
+    console.log(++askNumb);
+}
